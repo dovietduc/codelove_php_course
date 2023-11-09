414ecdb1764f2154de11e0c6b54abe572b6e6151
duc do <tvietduc122@gmail.com>
2023-11-09 16:32:50 +0700

    add index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ class View {
     {
 
         $dirRoot = __DIR__;
-        $pathView = $dirRoot . '/' . $path;
+        $pathView = $dirRoot . '/views/' . $path;
 
         // tao ra bien tuong ung voi value khi loop qua moi phan tu mang
         extract($data);
@@ -38,7 +38,7 @@ class ProductController {
         // 2. Hiển thị dữ liệu lấy được ra view
         $view = new View();
         $view->render(
-            'views/product/index.php',
+            'product/index.php',
             [
                 'products' => $products
             ]
@@ -52,3 +52,5 @@ class ProductController {
 // Instance Controller
 $productController = new ProductController();
 $productController->index();
+
+// routing -> math (path) --> action (controller)
